2d6e9ff5a3f40fb11670e2e85d58825f974407ff
Night-coder <122716766+akshit-pundir@users.noreply.github.com>
2025-09-27 12:19:23 +0530

    Update index.js for render

--- index.js
+++ index.js
@@ -8,13 +8,17 @@ const app = express();
 
 app.use(express.static(__dirname));
 
-const key = fs.readFileSync('cert.key');
-const cert = fs.readFileSync('cert.crt');
+// const key = fs.readFileSync('cert.key');
+// const cert = fs.readFileSync('cert.crt');
 
-const server = https.createServer({ key, cert }, app);
+// const server = https.createServer({ key, cert }, app);
+
+const server = app.listen(3000,()=>{
+    console.log("server is live on port 3000");    
+); 
 const io = socketIo(server);
 
-server.listen(3000);
+// server.listen(3000);
 
 // ************* SOCKET IO **************
 
